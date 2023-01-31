f58c75208c0f86d4a696b40a4585b3d7b5ab62ec
Waminms <67516571+Waminms@users.noreply.github.com>
2023-01-31 16:59:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "A";
+        String str2 = "B";
+        String str3 = "C";
+
+        StackContainer myStack = new StackContainer();
+        myStack.addToStack(str1);
+        myStack.addToStack(str2);
+        myStack.addToStack(str3);
+        System.out.println(myStack);
+
+        myStack.removeLast();
+        System.out.println(myStack);
+
+        System.out.println(myStack.getLast());
+        System.out.println(myStack.copyLast());
+
+        System.out.print("One reference for two objects: " );
+        System.out.println(myStack.getLast() == myStack.copyLast());
+        System.out.print("Two equivalent objects: " );
+        System.out.println(myStack.getLast().equals(myStack.copyLast()));
+    }
+}
---
+++ src/StackContainer.java
@@ -0,0 +1,81 @@
+import java.io.*;
+
+public class StackContainer {
+    private Object[] sc = new Object[4];
+    private int topOfTheStack = -1;
+
+    public StackContainer(Object[] sc) {
+        this.sc = sc;
+    }
+
+    public StackContainer(Object[] sc, int topOfTheStack) {
+        this.sc = sc;
+        this.topOfTheStack = topOfTheStack;
+    }
+
+    public StackContainer() {
+    }
+
+    public Object[] getSc() {
+        return sc;
+    }
+
+    public void setSc(Object[] sc) {
+        this.sc = sc;
+    }
+
+    public void addToStack(Object o) {
+        for (int i = 0; i < sc.length; i++) {
+            if (sc[i] == null) {
+                sc[i] = o;
+                topOfTheStack += 1;
+                return;
+            }
+        }
+    }
+
+    public Object getLast() {
+        return sc[topOfTheStack];
+    }
+
+    public void removeLast() {
+        sc[topOfTheStack] = null;
+        topOfTheStack -= 1;
+    }
+
+    public Object copyLast() {
+        Object lastObj = sc[topOfTheStack];
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("temp.txt"))) {
+            oos.writeObject(lastObj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Object lastObjCopy = null;
+        try (ObjectInputStream ois = new ObjectInputStream (new FileInputStream("temp.txt"))) {
+            lastObjCopy = ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        File f = new File("temp.txt");
+        f.delete();
+
+        return lastObjCopy;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < sc.length; i++) {
+            if (sc[i] != null) {
+                result += sc[i];
+                if (sc[i + 1] != null) {
+                    result += ",";
+                }
+            }
+        }
+        return "StackContainer{" +
+                "sc=" + result +
+                '}';
+    }
+}
